3bb6657125dba8241b91cce06b136981a2d4cb54
jtyuan <ws02298867@gmail.com>
2015-12-21 22:48:10 +0800

    initial push

---
+++ desktop/src/edu/eecs/pku/grail/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.eecs.pku.grail.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.eecs.pku.grail.AsteriatedGrailGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AsteriatedGrailGame(), config);
+	}
+}
---
+++ ios/src/edu/eecs/pku/grail/IOSLauncher.java
@@ -0,0 +1,22 @@
+package edu.eecs.pku.grail;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import edu.eecs.pku.grail.AsteriatedGrailGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new AsteriatedGrailGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
